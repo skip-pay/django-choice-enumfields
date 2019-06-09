335cff55ca504681c8abcdd405b5e2f531bceee8
Luboš Mátl <matllubos@gmail.com>
2019-06-09 21:37:20 +0200

    1.0.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django-choice-enumfields',
-    version='1.0.2',
+    version='1.0.3',
     author='HZDG, Lubos Matl',
     author_email='matllubos@gmail.com',
     description='Real Python Enums for Django.',
